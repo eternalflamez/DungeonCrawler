51dbbcc6adde06b223b40db75a397b661dcef1c2
Ron <unknown-user@fe80::8d93:2f8b:94bf:540%12>
2014-05-07 13:23:39 +0200

    Ik heb de players health toegevoegd

---
+++ Assets/Scripts/PlayerHealth1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth1 : MonoBehaviour {
+	
+	public int maxHealth = 100;
+	public int curHealth = 100;
+	
+
+	
+	public float healthBarLength;
+	
+	// Use this for initialization
+	void Start () {   
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AddjustCurrentHealth(0);
+	}
+	
+
+	
+	public void AddjustCurrentHealth(int adj){
+		
+		curHealth += adj;
+		
+		if(curHealth <0)
+			curHealth = 0;
+		
+		if(curHealth > maxHealth)
+			curHealth = maxHealth;
+		
+		if(maxHealth <1)
+			maxHealth = 1;
+
+		if (curHealth == 0){
+			//destroy player
+		}
+		
+		healthBarLength =(Screen.width /2) * (curHealth / (float)maxHealth);
+	}
+}
