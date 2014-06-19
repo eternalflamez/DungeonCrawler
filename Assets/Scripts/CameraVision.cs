6d8ed4b8c6f0b769bf90db4e6afd80640efbbb2d
Sander <san_dman@hotmail.com>
2014-06-19 16:03:46 +0200

    Gravity aan speler toegevoegd

--- Assets/Scripts/CameraVision.cs
+++ Assets/Scripts/CameraVision.cs
@@ -4,59 +4,79 @@ using AssemblyCSharp;
 
 public class CameraVision : MonoBehaviour 
 {
-	// public GameObject invCamera;
+    private bool falling;
+    private int fallingTime;
 
 	void Start() {
-
+        falling = false;
+        fallingTime = 0;
 	}
 
 	// Update is called once per frame
 	void Update () {
-
-		// if(!invCamera.activeSelf)
+		if (Input.GetKey ("q")) 
 		{
-			if (Input.GetKey ("q")) 
-			{
-				transform.Rotate(0, -2, 0);
-			}
-			
-			if (Input.GetKey ("e")) 
-			{
-				transform.Rotate(0, 2, 0);
-			}
-
-			if(Input.GetKey("w"))
-			{
-				transform.position += transform.forward * Time.deltaTime * 5;
-			}
-
-			if(Input.GetKey("s"))
-			{
-				transform.position += transform.forward * -5 * Time.deltaTime;
-			}
-
-			if(Input.GetKey("d"))
-			{
-				transform.position += transform.right * Time.deltaTime * 5;
-			}
-			
-			if(Input.GetKey("a"))
-			{
-				transform.position += transform.right * -5 * Time.deltaTime;
-			}
+			transform.Rotate(0, -2, 0);
 		}
-		/*
-		if(Input.GetKeyDown("i"))
+			
+		if (Input.GetKey ("e")) 
 		{
-			if(invCamera.activeSelf)
-			{
-				invCamera.SetActive(false);
-			}
-			else
-			{
-				invCamera.SetActive(true);
-			}
+			transform.Rotate(0, 2, 0);
 		}
-		*/
+
+		if(Input.GetKey("w"))
+		{
+			transform.position += transform.forward * Time.deltaTime * 5;
+		}
+
+		if(Input.GetKey("s"))
+		{
+			transform.position += transform.forward * -5 * Time.deltaTime;
+		}
+
+		if(Input.GetKey("d"))
+		{
+			transform.position += transform.right * Time.deltaTime * 5;
+		}
+			
+		if(Input.GetKey("a"))
+		{
+			transform.position += transform.right * -5 * Time.deltaTime;
+		}
+
+        RaycastHit hit;
+        Ray ray = new Ray(this.transform.position, -this.transform.up);
+
+        if (!Physics.Raycast(ray, 1.84f))
+        {
+            fallingTime++;
+
+            if (!falling)
+            {
+                falling = true;
+            }
+
+            if (Physics.Raycast(ray, out hit, 40))
+            {
+                if (hit.distance > 1.83 + (this.transform.up * Time.deltaTime * fallingTime).y)
+                {
+                    this.transform.position -= this.transform.up * Time.deltaTime * fallingTime * .5f;
+                }
+                else
+                {
+                    this.transform.position -= new Vector3(0, hit.distance - 1.83f, 0);
+                }
+            }
+            else
+            {
+                // We fell off the level.
+            }
+            
+        }
+        else if (falling)
+        {
+            falling = false;
+            fallingTime = 0;
+        }
 	}
 }
